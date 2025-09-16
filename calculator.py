04f515c6ef9a51f183f08456b17fa7c7106927d7
Peter Woodard <pwoodard@mocs.flsouthern.edu>
2025-09-16 12:02:34 -0400

    Added error handling to calculator and supplied tests throught new tests.py file

--- calculator.py
+++ calculator.py
@@ -1,20 +1,43 @@
 import math;
 
 def add(x,y): 
-    return x+y;
+    try:
+        return float(x) + float(y)
+    except ValueError:
+        raise ValueError("Both inputs must be numbers")
 def subtract(x,y): 
-    return x-y;
+    try:
+        return float(x) - float(y)
+    except ValueError:
+        raise ValueError("Both inputs must be numbers")
 def multiply(x,y): 
-    return x*y;
+    try:
+        return float(x) * float(y)
+    except ValueError:
+        raise ValueError("Both inputs must be numbers")
 def divide(x,y):
-    if (y == 0):
-        raise ValueError("Can't divide by zero");
-    else: 
-        return x/y;
+    try:
+        x = float(x)
+        y = float(y)
+    except ValueError:
+        raise ValueError("Both inputs must be numbers")
+
+    if y == 0:
+        raise ValueError("Cannot divide by zero")
+    else:
+        return x / y
 def power(x,y): 
-    return x**y;
+    try:
+        return float(x) ** float(y)
+    except ValueError:
+        raise ValueError("Both inputs must be numbers")
 def square_root(x):
-    if (y == 0):
-        raise ValueError("Can't take square root of a negative number");
-    else: 
-        return math.sqrt(x);
+    try:
+        x = float(x)
+    except ValueError:
+        raise ValueError("Input must be a number")
+
+    if x < 0:
+        raise ValueError("Cannot take square root of a negative number")
+    else:
+        return math.sqrt(x)
