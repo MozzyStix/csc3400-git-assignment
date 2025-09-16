2905df956e25473e5e7fde7d649e6acb079e4573
Peter Woodard <pwoodard@mocs.flsouthern.edu>
2025-09-16 11:01:41 -0400

    Add advanced mathematical operations

--- calculator.py
+++ calculator.py
@@ -1,8 +1,20 @@
+import math;
+
 def add(x,y): 
     return x+y;
 def subtract(x,y): 
     return x-y;
 def multiply(x,y): 
     return x*y;
-def divide(x,y): 
-    return x/y;
+def divide(x,y):
+    if (y == 0):
+        raise ValueError("Can't divide by zero");
+    else: 
+        return x/y;
+def power(x,y): 
+    return x**y;
+def square_root(x):
+    if (y == 0):
+        raise ValueError("Can't take square root of a negative number");
+    else: 
+        return math.sqrt(x);
